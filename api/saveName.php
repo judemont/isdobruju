01146f9c8c00ea8f48b41ad85044a7745d040997
JdM <julien@rmbi.ch>
2024-10-31 20:09:42 +0100

    idkk

--- api/saveName.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-
-
-$username = $_GET["username"];
-$_SESSION["username"] = $username;
--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-function sendName() {
-    const username = document.getElementById("usernameInput");
-    fetch("api/saveName.php?username=" + username);
+function q1() {
+    const name = document.getElementById("usernameInput").value;
+    window.localStorage.setItem("name", name )
+    document.getElementById("q1").style.display = "none"
+    document.getElementById("q2").style.display = "block"
 }
 
-
+function q2() {
+    const answer = document.getElementById("q2").querySelector('input:checked').value;
+    if(answer == "lausanne") {
+        alert("BRAVO !");
+    } else {
+        alert("FAKE")
+    }
+}
 
 document.getElementById("q1").style.display = "block"
